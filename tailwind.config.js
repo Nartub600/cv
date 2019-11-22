8f3707192b1d36d03d044d4be720570b0557542d
Nartub600 <maimar@gmail.com>
2019-11-22 14:43:16 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'print': {'raw': 'print'},
+      },
+      colors: {
+        'blue': '#183f5f',
+        'brown': '#b88c60',
+        'black': '#22292f',
+        'grey': '#b8c2cc',
+
+        'transparent': 'transparent',
+      },
+      fontFamily: {
+        'josefin': [
+          'Josefin Sans',
+          'sans-serif'
+        ]
+      }
+    }
+  }
+}
